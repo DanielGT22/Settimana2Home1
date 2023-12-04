42c8bb1f56622cc7497e28a0bd8522a6dce77828
Daniel Grujic <DanielGT22@github.com>
2023-12-04 15:27:11 +0100

    Es1

--- src/EssercizioUno/Primo.java
+++ src/EssercizioUno/Primo.java
@@ -1,5 +1,6 @@
 package EssercizioUno;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Primo {
@@ -13,21 +14,39 @@ public class Primo {
                 arr[i] = random.nextInt(10) + 1;
             }
 
-            System.out.print("Array casuale: ");
+            System.out.print("Array random: ");
             for (int i : arr) {
                 System.out.print(i + " ");
             }
             System.out.println("Che numero vuoi inserire? ");
-            int nuovoNumero = scanner.nextInt();
+            int nuovoNumero;
+            try {
+                nuovoNumero = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Deve essere un numero Intero");
+                break;
+            }
             System.out.println("In che posizione? ");
-            int posizione = scanner.nextInt();
+            int posizione;
+             try {
+                 posizione = scanner.nextInt();
+             } catch (InputMismatchException e) {
+                 System.out.println("Deve essere un numero Intero");
+                 break;
+             }
+
 
             if (posizione >= 1 && posizione <= arr.length + 1) {
                 arr[posizione - 1] = nuovoNumero;
 
-                System.out.print("Nuovo Array: ");
-                for (int i : arr) {
-                    System.out.print(i + " ");
+                System.out.println("Nuovo Array: ");
+                for (int i = 0; i < arr.length; i++) {
+                    System.out.print(arr[i]);
+                    if (i == arr.length - 1) {
+                        System.out.println();
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             } else {
                 System.out.println("Numero supera la lunghezza del array");
